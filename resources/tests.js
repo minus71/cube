b96ab6dd7d592ad4762245d249d41cf0f374a476
Mario Minutolo <mario.minutolo@gmail.com>
2013-04-16 08:12:01 -0400

    just a small text correction

--- resources/tests.js
+++ resources/tests.js
@@ -8,7 +8,7 @@ function getTestState(actionsIndexes){
     return state;
 }
 
-test( "a basic test example", function() {
+test( "A basic test example", function() {
   var value = "hello";
   equal( value, "hello", "We expect value to be hello" );
 });
